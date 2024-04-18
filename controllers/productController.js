f9f4bb2c0910d9380a6c46aeceb23a138fc8a97c
Entony Maxwell <entonymaxwell01@gmail.com>
2024-04-18 15:52:56 -0300

    Criando metodos get, update e delet

--- controllers/productController.js
+++ controllers/productController.js
@@ -58,7 +58,56 @@ const productController = {
         } catch (error) {
             console.log(error);
         }
-    }
+    },
+
+    update: async (req,res) => {
+        try {
+            const product = {
+                name: req.body.name,
+                code: req.body.code,
+                describe: req.body.describe,
+                price: req.body.price,
+                stock: req.body.stock,
+                image: req.body.image,
+            }
+
+            const productExists = await ProductModel.findById(req.params.id);
+
+            if(!productExists){
+                res.status(404).json({msg: "Product not found"});
+                return;
+            }
+
+            if(!req.body.name || !req.body.code || !req.body.describe || !req.body.price || !req.body.stock || !req.body.image){
+                res.status(406).json({msg: "Missing required fields"});
+                return;
+            }
+
+            const newProduct = await ProductModel.findByIdAndUpdate(req.params.id,product);
+
+            res.status(200).json({newProduct,msg: "Product updated successfully"});
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    delete: async (req,res) => {
+        try {
+            const product = await ProductModel.findById(req.params.id);
+
+            if(!product){
+                res.status(404).json({msg: "Product not found"});
+                return;
+            }
+
+            const productDeleted = await ProductModel.findByIdAndDelete(req.params.id);
+            res.status(200).json({productDeleted,msg: "Product deleted successfully"});
+            
+        } catch (error) {
+            console.log(error);
+        }
+    },
 }; 
 
 
