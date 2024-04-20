d0d6951640ec2176ad9372ba7d9cce34c00f1281
Entony Maxwell <entonymaxwell01@gmail.com>
2024-04-20 09:34:46 -0300

    Adicionando metodo de listar um worker

--- controllers/workerController.js
+++ controllers/workerController.js
@@ -18,7 +18,30 @@ const workerController = {
         } catch (error) {
             console.log(error);
         }
-    }
+    },
+
+    getAll: async (req,res) => {
+        try {
+            const workers = await WorkerModel.find();
+            res.status(200).json({workers});
+        } catch (error) {   
+            console.log(error);
+        }
+    },
+
+    getOne: async (req,res) => {
+        try {
+            const worker = await WorkerModel.findById(req.params.id);
+
+            if(!worker){
+                return res.status(404).json({msg: "Worker not found"});
+            }
+
+            res.status(200).json({worker});
+        } catch (error) {
+            console.log(error);
+        }
+    },
 }
 
 
