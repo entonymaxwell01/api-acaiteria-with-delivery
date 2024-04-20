2c4eef9a16f1610dc7ec842c88bb04aeccdc7722
Entony Maxwell <entonymaxwell01@gmail.com>
2024-04-20 09:43:13 -0300

    Criando metodo de update de worker

--- controllers/workerController.js
+++ controllers/workerController.js
@@ -42,6 +42,36 @@ const workerController = {
             console.log(error);
         }
     },
+
+    update: async (req,res) => {
+        try {
+            const id = req.params.id;
+            const worker = {
+                name: req.body.name,
+                cpf: req.body.cpf,
+                password: req.body.password,
+                email: req.body.email,
+                phone: req.body.phone,
+                office: req.body.office,
+            };
+
+            const workerUpdate = await WorkerModel.findByIdAndUpdate(id);
+
+            if(!req.body.name || !req.body.cpf || !req.body.password || !req.body.email || !req.body.phone || !req.body.office){
+                res.status(400).json({msg: "Missing fields"});
+                return 
+            }
+            if(!worker){
+                res.status(404).json({msg: "Worker not found"});
+                return 
+            }
+
+            res.status(200).json({workerUpdate,msg: "Worker updated successfully"});
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
 }
 
 
